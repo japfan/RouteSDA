39f7f32817fa32c5b1f1e0aeb4793d67746fee14
HRamadan <habibiramadanofc@gmail.com>
2026-06-28 17:09:29 +0700

    kelas Main

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        // Menjalankan GUI di thread yang aman (Event Dispatch Thread)
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame frame = new MainFrame();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
